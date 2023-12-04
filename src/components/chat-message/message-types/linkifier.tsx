aaa87b16a21f7d98f8f96baa5cb302e798b0b81c
baha-a <bhlshrf@gmail.com>
2023-12-04 12:59:08 +0300

    Fix url regex

--- src/components/chat-message/message-types/linkifier.tsx
+++ src/components/chat-message/message-types/linkifier.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import Linkify from 'linkify-react';
 
-const Linkifier = (props: { message: string | undefined }): JSX.Element => {
-  const { message = '' } = props;
-  
-  const regex = /(http(s)?):\/\/([a-zõäöüA-ZÕÄÖÜ0-9.-]+[\\/]?)+$/;
-  
-  return (
-    <div>
-      <Linkify
-        options={{
-          attributes: { target: '_blank' },
-          defaultProtocol: 'https',
-          validate: {
-            url: (value: string) => regex.test(value),
-            email: false,
-          },
-        }}
-      >
-        {message}
-      </Linkify>
-    </div>
-  );
-};
+interface LinkifierProps {
+  message: string | undefined;
+}
+
+const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+const Linkifier: React.FC<LinkifierProps> = ({ message }) => (
+  <div>
+    <Linkify
+      options={{
+        attributes: { target: '_blank' },
+        defaultProtocol: 'https',
+        validate: {
+          url: (value: string) => regex.test(value),
+          email: false,
+        },
+      }}
+    >
+      {message || ''}
+    </Linkify>
+  </div>
+);
 
 export default Linkifier;
