bef0781d2e41ff218480e501426aef78d55d2bcb
baha-a <bhlshrf@gmail.com>
2024-04-08 17:50:58 +0200

    Rename a function

--- src/store/middlewares/session-storage-middleware.ts
+++ src/store/middlewares/session-storage-middleware.ts
@@ -1,7 +1,7 @@
 import { Middleware } from 'redux';
 import { SESSION_STORAGE_CHAT_ID_KEY } from '../../constants';
 import { endChat, generateForwardingRequest, initChat, resetNewMessagesAmount, resetState, setIsChatOpen } from '../../slices/chat-slice';
-import { clearStateVariablesFromSessionStorage } from '../../utils/state-management-utils';
+import { clearStateVariablesFromLocalStorage } from '../../utils/state-management-utils';
 import { setToLocalStorage } from '../../utils/local-storage-utils';
 
 const sessionStorageMiddleware: Middleware = () => (next) => (action) => {
@@ -9,8 +9,8 @@ const sessionStorageMiddleware: Middleware = () => (next) => (action) => {
   if (generateForwardingRequest.fulfilled.match(action) && action.payload[0].externalId) {
     setToLocalStorage(SESSION_STORAGE_CHAT_ID_KEY, action.payload[0].externalId);
   }
-  if (endChat.pending.match(action)) clearStateVariablesFromSessionStorage();
-  if (resetState.match(action)) clearStateVariablesFromSessionStorage();
+  if (endChat.pending.match(action)) clearStateVariablesFromLocalStorage();
+  if (resetState.match(action)) clearStateVariablesFromLocalStorage();
   if (setIsChatOpen.match(action)) setToLocalStorage("newMessagesAmount", 0);
   if (resetNewMessagesAmount.match(action)) setToLocalStorage("newMessagesAmount", 0);
 
